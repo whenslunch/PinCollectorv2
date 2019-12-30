1491c17cafbe234885f57600e87f3740f19aaa42
Tze Lin Ong <tzong@microsoft.com>
2019-12-30 14:32:26 -0800

    clean up code after changes to getting pin images

--- src/components/PinItemList.vue
+++ src/components/PinItemList.vue
@@ -2,7 +2,7 @@
     <div>
         <ul>
             <li v-for="pin in pins" :key="pin.itemNumber">
-                <img :src="'https://collections-svc-dn.azurewebsites.net/api/GetImage/'+pin.itemNumber" style="height:10%;" />
+                <img :src="'https://collections-svc-dn.azurewebsites.net/api/GetImage/'+pin.itemNumber"  />
                 <h3> {{pin.city}} </h3>
                 <p> {{pin.country}} </p>
             </li>
@@ -18,12 +18,10 @@ export default {
     data() {
         return {
             pins: [],
-            images: [],
         };
     },
     async created() {
         await this.loadPins();
-        await this.loadImages();
     },
     methods: {
         async loadPins() {
@@ -31,13 +29,6 @@ export default {
             this.pins = await dataprovider.getPinData();
 
         },
-        async loadImages() {
-
-            this.pins.map( (pin) => {
-                this.images[pin.itemNumber] = dataprovider.getPinImage(pin.itemNumber);
-            })
-
-        }
     },
 };
 </script>
