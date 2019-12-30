fbbce50fdc7d5d42abb2825259f21cbe15de92ac
Tze Lin Ong <tzong@microsoft.com>
2019-12-30 13:17:31 -0800

    loaded images, but TODO: resize them to avoid large downloads!

--- src/components/PinItemList.vue
+++ src/components/PinItemList.vue
@@ -2,6 +2,7 @@
     <div>
         <ul>
             <li v-for="pin in pins" :key="pin.itemNumber">
+                <img :src="'https://collections-svc-dn.azurewebsites.net/api/GetImage/'+pin.itemNumber" style="height:10%;" />
                 <h3> {{pin.city}} </h3>
                 <p> {{pin.country}} </p>
             </li>
@@ -17,15 +18,26 @@ export default {
     data() {
         return {
             pins: [],
+            images: [],
         };
     },
     async created() {
         await this.loadPins();
+        await this.loadImages();
     },
     methods: {
         async loadPins() {
+
             this.pins = await dataprovider.getPinData();
+
         },
+        async loadImages() {
+
+            this.pins.map( (pin) => {
+                this.images[pin.itemNumber] = dataprovider.getPinImage(pin.itemNumber);
+            })
+
+        }
     },
 };
 </script>
